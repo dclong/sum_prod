a20d952c7bef924b8b2813779cd5548f0edd980b
Ben Chuanlong Du <duchuanlong+git@gmail.com>
2014-07-21 18:06:10 -0400

    use for loop for convenience or writing results to file immediate after available

--- range_para.m
+++ range_para.m
@@ -1,4 +1,6 @@
 Import["sum_prod.m"];
 DistributeDefinitions[TwoAddends, TwoFactors, S1, P1, S2, SumProductPuzzle, SolutionNumber];
-rr = Table[ParallelSubmit[{i}, SumProductPuzzle[{2, i}]], {i, 61, 600}];
-Export["rr.txt", rr, "Table"];
+For[i = 61, i <= 63, i++,
+   n = SolutionNumber[{2, i}]; 
+   PutAppend[ToString[i] <> " " <> ToString[n], "rr.txt"];
+];
